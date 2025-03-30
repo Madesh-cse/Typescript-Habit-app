c481597741cbb6ce784080775fef920f230991dd
Madesh <madeshdex10@gmail.com>
2025-03-30 19:03:02 +0530

    update

--- src/Components/main/Suggestion.tsx
+++ src/Components/main/Suggestion.tsx
@@ -1,10 +1,22 @@
-
-
+import { useInputContext } from "../../Context/InputSubmissonContext"
+import { IoLockClosedOutline } from "react-icons/io5";
 function Suggestion() {
+
+  const{ submittedValue} = useInputContext()
+
   return (
     <section>
         <div className='Suggestion'>
-
+          <div className="suggestion-list">
+            <ul>
+              {submittedValue.map((list,index)=>(
+                <div className="list-name">
+                  <p><span><IoLockClosedOutline/></span> My list - Personal</p>
+                  <li key={index}>{list}</li>
+                </div>
+              ))}
+            </ul>
+          </div>
         </div>
     </section>
   )
