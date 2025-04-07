ed95d91b59027d2c1fdcf7ace3b2c385829865c8
Madesh <madeshdex10@gmail.com>
2025-04-07 20:43:29 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import "react-datepicker/dist/react-datepicker.css";
 import Home from './pages/Home/Home';
 import Personal from './pages/Personal/Personal';
 import './styles/main.scss';
 import { SubmissionProvider } from './Context/InputSubmissonContext';
 import Work from './pages/Work/Work';
+import WeekSchedules from './pages/WeekSchedules/WeekSchedules';
 
 function App() {
-
   return (
     <Router>
       <SubmissionProvider>
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home/>}></Route>
           <Route path='Personal' element={<Personal/>}></Route>
           <Route path='Work' element={<Work/>}></Route>
+          <Route path='weekschedule' element={<WeekSchedules/>}></Route>
         </Routes>
       </SubmissionProvider>
     </Router>
